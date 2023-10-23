e65e318349e3e5fc76b0d869ce29ede4bad9cd9f
mabrur-h <mabrur.h11@gmail.com>
2023-10-23 11:13:49 +0200

    pass 2nd stage

--- app/main.js
+++ app/main.js
@@ -1,11 +1,14 @@
 const net = require("net");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+const pongResponse = `+PONG\r\n`
 
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
   // Handle connection
+  connection.on('data', () => {
+    connection.write(pongResponse);
+    connection.end; 
+  })
 });
 
 server.listen(6379, "127.0.0.1");
